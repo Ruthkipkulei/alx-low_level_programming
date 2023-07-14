694f9444e4e76b53cf04e72092799b4d409a9aaa
Ruthkipkulei <ruthkipkulei100@gmail.com>
2023-07-14 07:49:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -A program that uses put fuction
+ * Return: 0 (means successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

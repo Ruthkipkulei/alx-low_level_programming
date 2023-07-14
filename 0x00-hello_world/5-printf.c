4a7820e540e833df6d7f9855e51b61298cdb5c27
Ruthkipkulei <ruthkipkulei100@gmail.com>
2023-07-14 08:13:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+include < stdio.h >
+
+/**
+ * main -A program that uses printf fuction
+ * Return: 0 (means successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

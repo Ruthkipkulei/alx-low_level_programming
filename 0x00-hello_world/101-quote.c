07d82c094e04c61b43e4ac92188f8fbf7f61ba2d
Ruthkipkulei <ruthkipkulei100@gmail.com>
2023-07-16 13:11:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * Main -A program that not use printf or put fuction
+ * main - A program that not use printf or put fuction
  * Return: 1(means successful)
  */
-int main(void) /*This is an entry point*/
+int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

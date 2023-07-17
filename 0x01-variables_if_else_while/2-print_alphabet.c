4b908a7b27fab1acc62c2efaf196e17923c3af00
Ruthkipkulei <ruthkipkulei100@gmail.com>
2023-07-17 10:50:25 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-print_a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: 0 (Suceesful)
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 97;
+	while (c < 123)
+	{
+	putchar('c');
+	}
+	return (0);
+}
+
+
